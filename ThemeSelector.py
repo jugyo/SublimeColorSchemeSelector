178b662eadac759c6cea8c4bbc6b535b2c2ea22c
jugyo <kkohno@gmail.com>
2012-11-20 12:53:49 +0900

    refactor

--- ThemeSelector.py
+++ ThemeSelector.py
@@ -6,22 +6,23 @@ class SelectThemeCommand(sublime_plugin.WindowCommand):
         themes = self.get_themes()
 
         def on_done(index):
-            if index < 0:
-              return
-            theme = themes[index]
-            self.set_theme(theme)
+            if index >= 0:
+                self.set_theme(themes[index])
 
         self.window.show_quick_panel(themes, on_done)
 
     def get_themes(self):
-        settings = sublime.load_settings('Preferences.sublime-settings')
-        current_theme = settings.get('color_scheme')
-        files = os.listdir(os.path.join(sublime.packages_path(), 'Color Scheme - Default'))
-        files = filter(lambda f: f.endswith('tmTheme'), files)
+        current_theme = self.load_settings().get('color_scheme')
+        files = filter(lambda f: f.endswith('tmTheme'), os.listdir(self.theme_dir()))
         return files
 
     def set_theme(self, theme):
-        theme_path = os.path.join("Packages", "Color Scheme - Default", theme)
-        settings = sublime.load_settings('Preferences.sublime-settings')
-        settings.set('color_scheme', theme_path)
+        theme_file = os.path.join(self.theme_dir(), theme)
+        self.load_settings().set('color_scheme', theme_file)
         sublime.save_settings('Preferences.sublime-settings')
+
+    def theme_dir(self):
+        return os.path.join(sublime.packages_path(), "Color Scheme - Default")
+
+    def load_settings(self):
+        return sublime.load_settings('Preferences.sublime-settings')
