dd8fdce52696930c3957ec7b98e053063646c811
Kazuyuki Kohno <kkohno@twitter.com>
2013-10-02 00:42:07 +0900

    fix to find color schemes by using sublime.find_resources

--- ColorSchemeSelector.py
+++ ColorSchemeSelector.py
@@ -4,64 +4,40 @@ from glob import iglob
 from random import choice
 
 class SelectColorSchemeCommand(sublime_plugin.WindowCommand):
-    DEFUALT_COLOR_SCHEMES = [
-        "All Hallow's Eve",
-        "Amy",
-        "Blackboard",
-        "Cobalt",
-        "Dawn",
-        "Eiffel",
-        "Espresso Libre",
-        "IDLE",
-        "LAZY",
-        "Mac Classic",
-        "MagicWB (Amiga)",
-        "Monokai Bright",
-        "Monokai",
-        "Pastels on Dark",
-        "Slush & Poppies",
-        "Solarized (Dark)",
-        "Solarized (Light)",
-        "SpaceCadet",
-        "Sunburst",
-        "Twilight",
-        "Zenburnesque",
-        "iPlastic"
-    ]
-
     def run(self, **kwargs):
-        color_schemes = self.DEFUALT_COLOR_SCHEMES
+        color_schemes = sublime.find_resources("*.tmTheme")
 
         def on_done(index):
             if index >= 0:
-                self.set_color_scheme(self.path_to_color_scheme(color_schemes[index]))
+                self.set_color_scheme(color_schemes[index])
 
         if 'random' in kwargs:
-            self.set_color_scheme(self.path_to_color_scheme(choice(color_schemes)))
+            self.set_color_scheme(choice(color_schemes))
         elif 'direction' in kwargs:
-            self.move_color_scheme(kwargs['direction'])
+            self.move_color_scheme(color_schemes, kwargs['direction'])
         else:
+            items = [[os.path.basename(_), _] for _ in color_schemes]
             if int(sublime.version()) > 3000:
-                self.window.show_quick_panel(color_schemes, on_done, 0, self.current_scheme_index(), on_done)
+                self.window.show_quick_panel(items, on_done, 0, self.current_scheme_index(color_schemes), on_done)
             else:
-                self.window.show_quick_panel(color_schemes, on_done, 0, self.current_scheme_index())
+                self.window.show_quick_panel(items, on_done, 0, self.current_scheme_index(color_schemes))
 
     def path_to_color_scheme(self, name):
         return "Packages/Color Scheme - Default/%s.tmTheme" % name
 
-    def move_color_scheme(self, direction):
-        current_index = self.current_scheme_index()
+    def move_color_scheme(self, color_schemes, direction):
+        current_index = self.current_scheme_index(color_schemes)
         if direction == 'previous':
             index = current_index - 1
         elif direction == 'next':
-            index = current_index + 1 if current_index < len(self.DEFUALT_COLOR_SCHEMES) else 0
+            index = current_index + 1 if current_index < len(color_schemes) else 0
         else:
             raise ValueError
-        self.set_color_scheme(self.path_to_color_scheme(self.DEFUALT_COLOR_SCHEMES[index]))
+        self.set_color_scheme(color_schemes[index])
 
-    def current_scheme_index(self):
+    def current_scheme_index(self, color_schemes):
         current_scheme = self.load_settings().get('color_scheme')
-        return [self.path_to_color_scheme(c) for c in self.DEFUALT_COLOR_SCHEMES].index(current_scheme)
+        return [c for c in color_schemes].index(current_scheme)
 
     def set_color_scheme(self, color_scheme_path):
         self.load_settings().set('color_scheme', color_scheme_path)
